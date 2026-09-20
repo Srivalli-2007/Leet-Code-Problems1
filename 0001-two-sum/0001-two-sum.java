46e501ad761d2115d76fcb92e40c1fff647d27a6
Srivalli-2007 <sreevallinalli@gmail.com>
2026-09-20 21:50:20 +0530

    Time: 3 ms (59.05%), Space: 46.6 MB (93.58%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++) {
+            int need = target - nums[i];
+            if(set.contains(need)) {
+                for(int j = 0; j < i; j++) {
+                    if(nums[j] == need) {
+                        return new int[]{j, i};
+                    }
+                }
+            }
+            set.add(nums[i]);
+        }
+        return new int[]{};
+    }
+}
